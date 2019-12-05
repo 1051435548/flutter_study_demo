6dfdc40019493fc16144cc7720773e36b11bbbf0
yangbo <1051435548@qq.com>
2019-12-05 18:08:52 +0800

    fix bug

--- lib/common/Global.dart
+++ lib/common/Global.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/material.dart';
 
 
 class Global {
-  // 可选的主题列表
-  static List<MaterialColor> get themes => _themes;
 
   // 是否为release版
   static bool get isRelease => bool.fromEnvironment("dart.vm.product");
