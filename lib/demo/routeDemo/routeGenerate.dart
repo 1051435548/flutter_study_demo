4d4841aa0593561bbf7be687678575a6211b0353
yangbo <1051435548@qq.com>
2019-12-18 15:41:22 +0800

    1. 解决输入框出错的问题（当TextField处于Row下的子元素时，需要在外层包裹一个Expanded或者...不然会因为尺寸不明而报错）

--- lib/demo/routeDemo/routeGenerate.dart
+++ lib/demo/routeDemo/routeGenerate.dart
@@ -1,25 +1,117 @@
-import 'package:Flutter/community/bottomNavigationBar/three.dart';
+import 'package:Flutter/utils/ToastUtil.dart';
 import 'package:flutter/material.dart';
 
-class RouteGenerate extends StatelessWidget {
+class RouteGenerate extends StatefulWidget {
+  @override
+  _RouteGenerateState createState() => _RouteGenerateState();
+}
+
+class _RouteGenerateState extends State<RouteGenerate> {
+  bool hasText = false;
+  FocusNode focusNode = FocusNode();
+  TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Text('generate'),
+        automaticallyImplyLeading: false,
+        title: _title(),
       ),
       body: Container(
-          child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('generate'),
-            SizedBox(
-              height: 20.0,
-            ),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('skip to generate route page'),
+              SizedBox(
+                height: 20.0,
+              ),
+            ],
+          ),
         ),
-      )),
+      ),
     );
+  }
+
+  Widget _title() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width - 86,
+          height: 40,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: null,
+              ),
+              Expanded(
+                child: TextField(
+                  autofocus: true,
+                  controller: searchController,
+                  focusNode: focusNode,
+                  onSubmitted: (str) {
+                    focusNode.unfocus();
+                  },
+                  onChanged: (str) {
+                    if (searchController.text.length != 0) {
+                      setState(() {
+                        hasText = true;
+                      });
+                    } else {
+                      setState(() {
+                        hasText = false;
+                      });
+                    }
+                  },
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: '请输入搜索内容...',
+                    hintStyle: TextStyle(fontSize: 14, color: Colors.grey[400]),
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: Icon(hasText ? Icons.clear : Icons.camera_alt),
+                color: Colors.grey,
+                onPressed: () {
+                  hasText ? _clear() : _camera();
+                },
+              )
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Text(
+            '取消',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _clear() {
+    searchController.clear();
+    setState(() {
+      hasText = false;
+    });
+  }
+
+  void _camera() {
+    ToastUtil.show(context: context, msg: "调用相机");
   }
 }
